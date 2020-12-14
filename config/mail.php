1027d9060bcb8c71d548c338cb2b0571ec3ec707
Nic <nic.goodman@hey.com>
2020-12-14 01:03:16 -0500

    added form

--- config/mail.php
+++ config/mail.php
@@ -83,8 +83,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'nic.goodman@hey.com'),
+        'name' => env('MAIL_FROM_NAME', 'nicgoodman.com'),
     ],
 
     /*
--- public/js/home.vue.js
+++ public/js/home.vue.js
@@ -3,7 +3,10 @@ var Home = {
     data() {
         return {
             coderAnimation: false,
-            designerAnimation: false
+            designerAnimation: false,
+            projectFilter: 'all',
+            showProject: false,
+            projectContent: ''
         };
     },
     computed: {},
@@ -13,6 +16,18 @@ var Home = {
         },
         toggleDesignerAnimation() {
             this.designerAnimation = !this.designerAnimation;
+        },
+        setProjectFilter(filter) {
+            this.projectFilter = filter;
+        },
+        async fetchProject(url) {
+           await fetch(url)
+            .then(response => response.text())
+            .then(data => (this.projectContent = data));
+            this.showProject = true;
+        },
+        toggleProject() {
+            this.showProject = !this.showProject;
         }
     }
 };
