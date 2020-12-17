9ec0d199851fe452826ab33f17ac561c25399915
Nic <nic.goodman@hey.com>
2020-12-17 16:06:21 -0500

    added all setting to projects, adjusted contact styling

--- public/js/home.vue.js
+++ public/js/home.vue.js
@@ -1,12 +1,14 @@
 var Home = {
-    template: '#work-content',
+    template: "#work-content",
     data() {
         return {
             coderAnimation: false,
             designerAnimation: false,
-            projectFilter: 'all',
+            projectFilter: "all",
             showProject: false,
-            projectContent: ''
+            projectContent: "",
+            showClass: 'opacity-100 max-h-full visible max-w-full m-6 md:m-8',
+            hideClass: 'opacity-0 max-h-0 invisible max-w-0 m-0 overflow-hidden'
         };
     },
     computed: {},
@@ -18,20 +20,27 @@ var Home = {
             this.designerAnimation = !this.designerAnimation;
         },
         setProjectFilter(filter) {
-            if ( this.projectFilter === filter ) {
-                this.projectFilter = 'all'
+            if (this.projectFilter === filter) {
+                this.projectFilter = "all";
             } else {
                 this.projectFilter = filter;
             }
         },
         async fetchProject(url) {
-           await fetch(url)
-            .then(response => response.text())
-            .then(data => (this.projectContent = data));
+            await fetch(url)
+                .then(response => response.text())
+                .then(data => (this.projectContent = data));
             this.showProject = true;
         },
         toggleProject() {
             this.showProject = !this.showProject;
+        },
+        filterProjects(type) {
+            if ( this.projectFilter === type || type === 'all' || this.projectFilter === 'all' ) {
+                return this.showClass;
+            } else {
+                return this.hideClass;
+            }
         }
-        }
+    }
 };
