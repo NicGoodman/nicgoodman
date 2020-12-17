40ff3e30d1017df27d41e7b16b58954aa91ab106
Nic <nic.goodman@hey.com>
2020-12-16 20:24:15 -0500

    testing axios

--- public/js/contact.vue.js
+++ public/js/contact.vue.js
@@ -2,21 +2,45 @@ var Contact = {
     template: "#contact-content",
     data() {
         return {
-        };
+			formData: {
+			name: null,
+			email: null,
+			phone: null,
+			message: null
+			},
+        }
     },
     methods: {
-        processForm() {
-                let myForm = document.getElementById("contactForm");
-                let formData = new FormData(myForm);
-                fetch("/", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    },
-                    body: new URLSearchParams(formData).toString()
-                })
-                    .then(() => console.log("Form successfully submitted"))
-                    .catch(error => alert(error));
+        encode(data) {  
+            const formData = new FormData();
+            
+            for (const key of Object.keys(data)) {
+                formData.append(key, data[key]);
             }
+            
+            return formData;
+        },
+
+        handleFormSubmit(e) {
+            const axiosConfig = {
+                header: { "Content-Type": "application/x-www-form-urlencoded" }
+            };
+
+            axios.post(
+                location.href, 
+                this.encode({
+                    'form-name': e.target.getAttribute("name"),
+                    ...this.formData,
+                }),
+                axiosConfig
+            )
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+            .then(document.getElementById("contactForm").innerHTML = `
+            <div>
+                Thank you! I received your submission.
+            </div>
+            `)
+        }
     }
 };
