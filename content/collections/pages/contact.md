9ff87a4a90ee6974e0410c5c4a1ff7a066221422
Nic <nic.goodman@hey.com>
2020-12-16 19:29:07 -0500

    added contact form

--- content/collections/pages/contact.md
+++ content/collections/pages/contact.md
@@ -2,9 +2,16 @@
 title: Contact
 author: b8cab3f6-baa5-4311-822b-8ad388e79916
 updated_by: b8cab3f6-baa5-4311-822b-8ad388e79916
-updated_at: 1607989045
+updated_at: 1608163651
 template: templates/contact
 layout: layouts/basic-layout
 hero_background: coder_background-02.svg
+contact_intro:
+  -
+    type: paragraph
+    content:
+      -
+        type: text
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 id: 298473d7-1bca-49e3-9257-a2852c79a4c6
 ---
--- public/js/contact.vue.js
+++ public/js/contact.vue.js
@@ -1,3 +1,32 @@
 var Contact = { 
 	template: "#contact-content"
 };
+
+function sendFormData() {
+
+	function sendData() {
+	  var XHR = new XMLHttpRequest()
+	  var FD = new FormData(form)
+	  XHR.addEventListener('load', function(event) {
+		form.classList.add('inactive')
+		var success = document.querySelectorAll('.post-sent')[0]
+		success.classList.add('active')
+	  })
+	  XHR.addEventListener('error', function(event) {
+		form.classList.add('inactive')
+		var error = document.querySelectorAll('.post-error')[0]
+		error.classList.add('active')
+	  })
+	  XHR.open('POST', '#')
+	  XHR.send(FD)
+	}
+  
+	var form = document.querySelectorAll('#contactForm')[0]
+	form.addEventListener('submit', function (e) {
+	  e.preventDefault()
+	  sendData()
+	})
+  
+  }
+  
+  export default sendFormData
