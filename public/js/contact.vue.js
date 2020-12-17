1bc239d604513c9ef3c26872e2451ba329a3aaaf
Nic <nic.goodman@hey.com>
2020-12-16 19:59:20 -0500

    teseting netlify form

--- public/js/contact.vue.js
+++ public/js/contact.vue.js
@@ -1,31 +1,25 @@
-var Contact = { 
-	template: "#contact-content",
-	methods: {
-		sendFormData() {
-
-			sendData() {
-			  var XHR = new XMLHttpRequest()
-			  var FD = new FormData(form)
-			  XHR.addEventListener('load', function(event) {
-				form.classList.add('inactive')
-				var success = document.querySelectorAll('.post-sent')[0]
-				success.classList.add('active')
-			  })
-			  XHR.addEventListener('error', function(event) {
-				form.classList.add('inactive')
-				var error = document.querySelectorAll('.post-error')[0]
-				error.classList.add('active')
-			  })
-			  XHR.open('POST', '#')
-			  XHR.send(FD)
-			};
-		  
-			var form = document.querySelectorAll('#contactForm')[0]
-			form.addEventListener('submit', function (e) {
-			  e.preventDefault()
-			  sendData()
-			})
-		  
-		  }
-	}
+var Contact = {
+    template: "#contact-content",
+    data() {
+        return {
+        };
+    },
+    methods: {
+        processForm() {
+            const handleSubmit = e => {
+                e.preventDefault();
+                let myForm = document.getElementById("contactForm");
+                let formData = new FormData(myForm);
+                fetch("/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: new URLSearchParams(formData).toString()
+                })
+                    .then(() => console.log("Form successfully submitted"))
+                    .catch(error => alert(error));
+            };
+        }
+    }
 };
