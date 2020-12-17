262af854e5b5bf4598fee27eb02f69b3bc042fef
Nic <nic.goodman@hey.com>
2020-12-17 18:05:47 -0500

    added initial animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,15 +21,36 @@ module.exports = {
                 yellow: '2px solid #FBBF24'
             },
             keyframes: {
-              hoverBounce: {
+              hoverBounceUp: {
                 '0%': { transform: 'translateY(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' },
                 '50%': { transform: 'translateY(-10%)', opacity: '0.66', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'},
                 '100%': { transform: 'translateY(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' }
-
-              }
+              },
+              hoverBounceDown: {
+                '0%': { transform: 'translateY(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' },
+                '50%': { transform: 'translateY(10%)', opacity: '0.66', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'},
+                '100%': { transform: 'translateY(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' }
+              },
+              hoverBounceLeft: {
+                '0%': { transform: 'translateX(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' },
+                '50%': { transform: 'translateX(-10%)', opacity: '0.66', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'},
+                '100%': { transform: 'translateX(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' }
+              },
+              hoverBounceRight: {
+                '0%': { transform: 'translateX(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' },
+                '50%': { transform: 'translateX(10%)', opacity: '0.66', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'},
+                '100%': { transform: 'translateX(0)', opacity: '1', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' }
+              },
             },
             animation: {
-              hoverBounce: 'hoverBounce 1s infinite ease-in-out'
+              hoverBounceUp: 'hoverBounceUp 1s infinite ease-in-out',
+              hoverBounceDown: 'hoverBounceDown 1s infinite ease-in-out',
+              hoverBounceLeft: 'hoverBounceLeft 1s infinite ease-in-out',
+              hoverBounceRight: 'hoverBounceRight 1s infinite ease-in-out',
+              initialBounceUp: 'hoverBounceUp 1s 3 ease-in-out',
+              initialBounceDown: 'hoverBounceDown 1s 3 ease-in-out',
+              initialBounceLeft: 'hoverBounceLeft 1s 3 ease-in-out',
+              initialBounceRight: 'hoverBounceRight 1s 3 ease-in-out'
             }
         },
         fontFamily: {
